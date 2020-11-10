6a7f1917276b023ba46955ae1dc993634bbcd5d3
Ferreol <ferreol.soulez@univ-lyon1.fr>
2020-11-10 17:48:13 +0100

    Update large image files

--- Data/Simus/GrndTruth/SimuOrion.m
+++ Data/Simus/GrndTruth/SimuOrion.m
@@ -1,5 +1,5 @@
-Imphase = (imread('~/matlab/COMCI/Data/Simus/GrndTruth/OrionNebula_Amp_Ph.tif','Index',2));
-ImMod = (imread('~/matlab/COMCI/Data/Simus/GrndTruth/OrionNebula_Amp_Ph.tif','Index',1));
+Imphase = (imread('Data/Simus/GrndTruth/OrionNebula_Amp_Ph.tif','Index',2));
+ImMod = (imread('Data/Simus/GrndTruth/OrionNebula_Amp_Ph.tif','Index',1));
 
 %w1 = window(@tukeywin,18000,0.002);
 w1 =  1;
@@ -9,23 +9,3 @@ clear ImMod Imphase
 padsz=1080;
 truth = padarray(truth,[padsz, padsz],'symmetric');
 szt = size(truth);
-
-% nAngle = 1; 
-% Iangle = zeros(2,nAngle);
-% lambda= 530e-9; % wavelenght
-% n0 = 1.0;
-% z = 250*1e-6;
-% %dxy = 5.2e-6; % Camera  pixel size
-% dxy= 1.04e-6 ;
-% 
-% 
-% % super resolution factor
-% sr=10; % dxy / dxyin
-% 
-% S = LinOpShape( [szx  ],[sr  szx(1)/sr  sr szx(2)/sr nAngle]);
-% %Scaling(1./sr)
-% Frwrd =LinOpDiag([sr  szx(1)/sr  sr szx(2)/sr nAngle],1./sr)* S*LinOpPropagator(szx,lambda, n0, z,dxy/sr,  Iangle, 1,1,'AS');
-% 
-% y = Frwrd* data;
-% iy =(abs(y)).^2;
-% d1 = squeeze( sum( sum( iy(:,:,:,:,:),3),1));
--- Data/Simus/GrndTruth/SimuUSAFph.m
+++ Data/Simus/GrndTruth/SimuUSAFph.m
@@ -1,5 +1,8 @@
-inim = double(imread('GrndTruth/USAF-1951_65nm_tilt_C.png'))/255.;
-inim = inim(4135:4134+20160,3857:3856+20160);
+%inim = double(imread('GrndTruth/USAF-1951_65nm_tilt_C.png'))/255.;
+%inim = inim(4135:4134+20160,3857:3856+20160);
+inim = double(imread('Data/Simus/GrndTruth/USAF-1951_104nm_tilt_C.png'))/255.;
+inim = inim(521:520+20160,529:528+20160);
+
 truth=exp(1i.*inim);
 clear inim;
 
