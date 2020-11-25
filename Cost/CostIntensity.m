e5fb5f297f36359c8a2d7c1e9b24f79868a9507f
Ferreol <ferreol.soulez@univ-lyon1.fr>
2020-11-25 15:27:14 +0100

    CostIntensity: Remove Safe implementation and fix a bug in 'Poisson' when shot=0

--- Cost/CostIntensity.m
+++ Cost/CostIntensity.m
@@ -140,172 +140,7 @@ classdef CostIntensity < Cost
             
             
         end
-        
-        function cost = applySafe(this,x) % get the function cost
-            
-            
-            Ix = abs(x).^2;
-            if (~isempty(this.index))
-                for n=this.index
-                    Ix = sum(Ix,n);
-                end
-            end
-            if (~isempty(this.widx))
-                Ix = Ix(this.widx);
-            else
-                Ix = Ix(:);
-            end
-            
-            switch(this.noisemodel)
-                case {0,1} % Gaussian noise
-                    
-                    
-                    res = this.w.* ( Ix - this.y).^2;
-                    cost = sum(res(:));
-                case {2,3} % Poisson noise
-                    Ix = Ix(:) + this.w(:);
-                    t = (this.y==0);
-                    cost = this.Pcost;
-                    if any(t)
-                        cost = cost + sum(Ix(t) );
-                    end
-                    if any(~t)
-                        cost = cost + sum(Ix(~t) -  this.y(~t) .* log(Ix(~t)));
-                    end
-                case 4 % Laplace noise
-                    res = abs( abs(x(:)).^2 - this.y);
-                    cost = sum(res(:));
-            end
-        end
-    
-        function z = applyProxSafe(this,x,alpha) % Apply the operator
-            %   assert(isscalar(alpha),'alpha must be a scalar');
-            assert( isnumeric(x),  'x should be numeric');
-            
-            this.szx = size(x);
-            
-            ndms = length(this.szx);
-            % Special case for vectors as matlab thought it is matrix ;-(
-            if this.szx(2) ==1
-                ndms = 1;
-            end
-            T = true(ndms,1);
-            T(this.index)=false;
-            this.kerdims = this.szx;
-            this.kerdims(T)=1;
-            this.imdims = this.szx;
-            this.imdims(~T)=1;
-            
-            
-            z=x;
-            x=abs(x);
-            %     y = y./x;
-            if (~isempty(this.index))
-                nx = x.^2;
-                for n=this.index
-                    nx = (sum(nx,n));
-                end
-                nx = sqrt(nx);
-                x = nx;
-            else
-                nx= x;
-            end
-            
-            if (~isempty(this.widx))
-                ymodt = zeros_(numel(x),1);
-                ymodt(~this.widx) =x(~this.widx);
-                x = x(this.widx);
-                if (~isscalar(alpha))
-                    alpha = alpha(this.widx);
-                end
-            end
-            x = x(:);
-            ymod = zeros_(numel(x),1);
-            
-            switch this.noisemodel
-                case {0,1} % Gaussian case
-                    % Amounts to find the hight real root of
-                    % x^3 - 3q x = 2r;
-                    q = -1./3* ( 1./(4*alpha * this.w) - this.y);
-                    r = -0.5*x./(alpha*4*this.w);
-                    p = q.^3 - r.^2;
-                    
-                    t = (p>=0);
-                    % Case p>0 % three real roots
-                    if any(t)
-                        qt = sqrt(q(t));
-                        theta = acos(r(t)./(qt.^3))./3.0;
-                        ymod(t)= -2* qt.* min( min(cos(theta),cos(theta+2*pi/3)),cos(theta+4*pi./3));
-                        
-                    end
-                    
-                    if any(~t)
-                        sr = sign(r(~t));
-                        sr(sr==0)=1; % assume 0 is positive
-                        pt =  sr.*nthroot( sqrt(-p(~t)) + abs(r(~t)) , 3);
-                        ymod(~t) = -pt-q(~t)./pt;
-                    end
-                    %
-                case 2 % Poisson noise without background (solve a quadratic equation)
-                    b = alpha*x;
-                    ymod = 0.5./(2+ alpha)*(b+sqrt(b.^2+ 8 * (2+ alpha) * this.y));
-                    
-                case 3 % Poisson noise + background
-                    % the solution is the highest real roots of
-                    % x^3 + a2 x^2 + a1 x + a0
-                    
-                    a2 = -1./(1 + 2.*alpha).*x;
-                    a1 = this.w - 2.*alpha./(1+2.*alpha) .* this.y;
-                    a0 = a2.* this.w;
-                    
-                    q = -1./9. .*( 3.*a1 - a2.^2);
-                    r = -1./54. .* (9.*a1.*a2 - 27.* a0 - 2.* a2.^3);
-                    
-                    p = q.^3 - r.^2;
-                    
-                    t = (p>=0);
-                    % Case p>0 % three real roots
-                    if any(t)
-                        qt = sqrt(q(t));
-                        theta = acos(r(t)./(qt.^3))./3.0;
-                        ymod(t)= -2* qt.* min( min(cos(theta),cos(theta+2*pi/3)),cos(theta+4*pi./3))- 1./3..*a2(t);
-                        
-                    end
-                    
-                    if any(~t)
-                        sr = sign(r(~t));
-                        sr(sr==0)=1; % 0 is positive
-                        pt =  sr.*nthroot( sqrt(-p(~t)) + abs(r(~t)) , 3);
-                        ymod(~t) = -pt-q(~t)./pt- 1./3..*a2(~t);
-                    end
-                case 4 % Laplace
-                    ymod = sqrt(this.y);
-                    st  = sign(x.^2 - this.y);
-                    t = (st==1);
-                    ymod(t) = max( 1./(2.*alpha+1).*x(t), sqrt(this.y(t)));
-                    
-                    if alpha<0.5
-                        t = (st==-1);
-                        ymod(t) = min( 1./(2.*alpha-1).*x(t), sqrt(this.y(t)));
-                    end
-                    
-                    
-            end
-            
-            if (~isempty(this.widx))
-                ymodt(this.widx) = ymod;
-                ymod= ymodt;
-            end
-            
-            
-            znx = (nx~=0);
-            if (any(znx))
-                ymod(znx)= ymod(znx)./nx(znx);
-            end
-            z = z.* reshape(repmat(reshape(ymod,this.imdims),this.kerdims),this.szx); % y = x ./ |x| * ymod
-            
-        end
-    end 
+    end
     methods (Access = protected)
         function z = applyProx_(this,x,alpha) % Apply the operator
             %   assert(isscalar(alpha),'alpha must be a scalar');
@@ -353,7 +188,7 @@ classdef CostIntensity < Cost
             
             switch this.noisemodel
                 case {0,1} % Gaussian case
-                    % Amounts to find the hight real root of
+                    % Amounts to find the highest real root of
                     % x^3 - 3q x = 2r;
                     q = -1./3* ( 1./(4*alpha * this.w) - this.y);
                     r = -0.5*x./(alpha*4*this.w);
@@ -376,9 +211,8 @@ classdef CostIntensity < Cost
                     end
                     %
                 case 2 % Poisson noise without background (solve a quadratic equation)
-                    b = alpha*x;
-                    ymod = 0.5./(2+ alpha)*(b+sqrt(b.^2+ 8 * (2+ alpha) * this.y));
-                    
+                    ymod = 1/(2+4.*alpha).*(x + sqrt(8.*alpha.*(1+2.*alpha).*this.y+x.^2));
+                                        
                 case 3 % Poisson noise + background
                     % the solution is the highest real roots of
                     % x^3 + a2 x^2 + a1 x + a0
